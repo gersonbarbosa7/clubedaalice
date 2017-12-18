2af6e8bae54543923fbcb198b0387711a46f6768
Gerson <gersonbarbosa7@gmail.com>
2017-12-18 03:37:39 -0300

    Nova home - Up arquivos base
    Iniciando montagem no projeto online

---
+++ wp-content/themes/alice/header-novahome.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<meta name="google-site-verification" content="o8Yan04vWOF0mi_yuh4TuW-nlc6yNKTUFkmfjor8XVE" />
+        <link rel="stylesheet" media="all" href="<?php echo get_template_directory_uri(); ?>/style.css?ver=all" />
+        <link rel="stylesheet" media="all" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '1693026067645807',
+	      xfbml      : true,
+	      version    : 'v2.6'
+	    });
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/pt_BR/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	</script>
+
+	<div id="fb-root"></div>	
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<?php 
+		get_template_part('includes/modules/module', 'top-bar'); 
+	?>
+	<header>
+		<div class="container">
+			<div class="row">	
+				<div class="hidden-xs">
+					<?php get_template_part('includes/modules/module', 'social-icons'); ?>			
+				</div>			
+
+				<a href="<?php echo home_url(); ?>" id="logo" title="<?php bloginfo('name'); ?>">
+					<img src="<?php echo IMG; ?>logo-clube-da-alice.png" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>">
+				</a>
+                            
+                                <?php if (is_user_logged_in()){ ?>
+                                 
+                            <ul id="login_topo" class="logado">
+                                    <li>
+                                        <a href="<?php echo home_url(); ?>/solicitar-carteirinha">Minha conta</a> 
+                                        <img src="<?php echo get_template_directory_uri(); ?>/images/icone-logado.png" class="img-responsive" />
+                                    </li>                                                                                                       
+                                </ul>
+                            
+                                <?php } else { ?>
+                                <ul id="login_topo">
+                                    <li><a href="javascript:void(0)" data-toggle="modal" data-target="#login_conta">Login</a></li>
+                                    <li><a href="javascript:void(0)">Criar conta</a></li>
+                                </ul>
+                                <?php } ?>
+
+				<div class="descontos-topo visible-xs">
+					<a href="<?php bloginfo('url'); ?>/descontos">
+						<img src="<?php bloginfo('template_directory'); ?>/images/descontos.png" alt="Descontos">
+					</a>
+				</div>
+
+				<a class="bottom hidden-xs hidden-sm" target="_blank" href="http://carteirinha.clubedaalice.com.br/">
+					<div class="chamada">
+						<h2>Vem pro</h2>
+						<p>Clube!</p>
+					</div>
+					<div class="imagem">
+						<img src="<?php echo IMG; ?>alice-bottom.png" alt="Clube Secreto">	
+					</div>					
+				</a>
+			</div>
+		</div>
+	</header>
+	<?php 
+		if (is_singular('post') || ((is_category() || is_archive()) && !is_tax('categoria_da_empresa')) || is_page('blog')) {
+			get_template_part('includes/modules/module', 'menu-blog');
+		}
+		else {
+			get_template_part('includes/modules/module', 'menu');
+		}
+	?>
+	
---
+++ wp-content/themes/alice/novahome.php
@@ -0,0 +1,109 @@
+<?php
+/* Template Name: Nova Home */
+get_template_part('header-novahome');
+?>
+<section id="banner_novahome">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-xs-12">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/bg-novahome.jpg" class="img-responsive" id="banner_desktop" alt="Banner Alice" />
+                <img src="<?php echo get_template_directory_uri(); ?>/images/bg-mobile.jpg" class="img-responsive" alt="Banner Alice" id="banner_mobile" style="display: none;" />
+            </div>
+        </div>
+    </div>
+</section>
+<section id="posts_detaque">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-xs-12">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/img-blog1.jpg" class="img-responsive" alt="Blog1" />
+                <div class="titulo_postdestaque text-center">
+                    <h3>VAMOS CORRER QUE O VERÃO ESTÁ CHEGANDO</h3>
+                    <span>ALICE EM FORMA</span>
+                </div>
+                <div class="box-textpost text-center">
+                    <p class="text-left">E o verão está chegando… Esta época do ano é a hora que começa a bater o desespero em muitas mulheres. O verão se aproxima e a hora de encarar o biquíni está cada vez mais perto. </p>
+                    <button class="btn btn-default text-center btn-rosa-alice">LEIA MAIS ></button>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/img-blog2.jpg" class="img-responsive" alt="Blog2" />
+                <div class="titulo_postdestaque text-center">
+                    <h3>VAMOS CORRER QUE O VERÃO ESTÁ CHEGANDO</h3>
+                    <span>ALICE EM FORMA</span>
+                </div>
+                <div class="box-textpost text-center">
+                    <p class="text-left">E o verão está chegando… Esta época do ano é a hora que começa a bater o desespero em muitas mulheres. O verão se aproxima e a hora de encarar o biquíni está cada vez mais perto. </p>
+                    <button class="btn btn-default text-center btn-rosa-alice">LEIA MAIS ></button>
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-12">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/img-blog3.jpg" class="img-responsive" alt="Blog3" />
+                <div class="titulo_postdestaque text-center">
+                    <h3>VAMOS CORRER QUE O VERÃO ESTÁ CHEGANDO</h3>
+                    <span>ALICE EM FORMA</span>
+                </div>
+                <div class="box-textpost text-center">
+                    <p class="text-left">E o verão está chegando… Esta época do ano é a hora que começa a bater o desespero em muitas mulheres. O verão se aproxima e a hora de encarar o biquíni está cada vez mais perto. </p>
+                    <button class="btn btn-default text-center btn-rosa-alice">LEIA MAIS ></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="banner_transforme">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-xs-12 text-center">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/banner-transforme.jpg" class="img-responsive" alt="Banner Transform" />
+            </div>
+        </div>
+    </div>
+</section>
+<section id="novo_miolo">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-xs-12">
+                <!--loop posts-->
+                <div class="col-sm-12 col-xs-12">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/blog1.jpg" class="img-responsive" alt="Banner Transform" />
+                    <div class="titulo_postdestaque text-center">
+                        <h3>VAMOS CORRER QUE O VERÃO ESTÁ CHEGANDO</h3>
+                        <span>ALICE EM FORMA</span>
+                    </div>
+                    <div class="box-textpost">
+                        <p>E o verão está chegando… Esta época do ano é a hora que começa a bater o desespero em muitas mulheres. O verão se aproxima e a hora de encarar o biquíni está cada vez mais perto. </p>
+                        <button class="btn btn-default text-center btn-rosa-alice">LEIA MAIS ></button>
+                    </div>
+                </div>
+                <!--/loop-->
+                
+                <!--loop posts-->
+                <div class="col-sm-12 col-xs-12">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/blog1.jpg" class="img-responsive" alt="Banner Transform" />
+                    <div class="titulo_postdestaque text-center">
+                        <h3>VAMOS CORRER QUE O VERÃO ESTÁ CHEGANDO</h3>
+                        <span>ALICE EM FORMA</span>
+                    </div>
+                    <div class="box-textpost">
+                        <p>E o verão está chegando… Esta época do ano é a hora que começa a bater o desespero em muitas mulheres. O verão se aproxima e a hora de encarar o biquíni está cada vez mais perto. </p>
+                        <button class="btn btn-default text-center btn-rosa-alice">LEIA MAIS ></button>
+                    </div>
+                </div>
+                <!--/loop-->
+            </div>
+            <div class="col-sm-4 col-xs-12" id="div_sidebar">
+                <div class="div_sidebar">
+                    <div class="fb-page" data-href="https://www.facebook.com/clubedaaliceoficial/?ref=ts&amp;fref=ts" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/clubedaaliceoficial/?ref=ts&amp;fref=ts" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/clubedaaliceoficial/?ref=ts&amp;fref=ts">Clube da Alice</a></blockquote></div>
+                </div>
+                <div class="div_sidebar">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/banner-inscreva-se.jpg" class="img-responsive" alt="Banner Transform" />
+                </div>
+                <div class="div_sidebar">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/banner-alice.jpg" class="img-responsive" alt="Banner Transform" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
